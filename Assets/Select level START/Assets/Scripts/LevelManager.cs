be8518dc1b5ad9b7d3754a9f78cc5e8d1f6eae97
FuuNuka <75213697+FuuNuka@users.noreply.github.com>
2026-03-28 16:19:16 +0700

    Thesis_Final_2024
    Body Tricking Thesis
    Score Code Media pipe : https://github.com/ganeshsar/UnityPythonMediaPipeAvatar

---
+++ Assets/Select level START/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+ 
+public class LevelManager : MonoBehaviour {
+ 
+	public void GoBackToLevelSelection() {
+        SceneManager.LoadScene(0);
+    }
+}
